d5d4dfa8092f9161734ebc224cca513e445ad199
heosangbeom <luv_3_u@naver.com>
2024-12-25 13:07:29 +0900

    feat(text): text 꾸미기
    - 텍스트 꾸미기

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,26 @@ import 'package:vid_52_player/screen/home_screen.dart';
 
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData(
+      fontFamily: 'sunflower',
+      textTheme: TextTheme(
+        displayLarge: TextStyle(
+            color: Colors.black, fontSize: 32.0, fontFamily: 'parisienne'),
+        displayMedium: TextStyle(
+          color: Colors.pink[100],
+          fontSize: 50.0,
+          fontWeight: FontWeight.w700,
+        ),
+        bodyLarge: TextStyle(
+          color: Colors.pink[100],
+          fontSize: 30.0,
+        ),
+        bodyMedium: TextStyle(
+          color: Colors.pink[100],
+          fontSize: 20.0,
+        ),
+      ),
+    ),
     home: HomeScreen(),
   ));
 }
-
